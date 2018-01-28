064b8ad0d5495faedd1747481c0fa23f4c9bc2c3
Aditya C <saiadityac@gmail.com>
2018-01-27 20:45:14 -0800

    lookup neighbourhood of 13

--- naive_bot.py
+++ naive_bot.py
@@ -14,16 +14,23 @@ enemy_differences = [[0, 0] for u in range(player_count)]
 my_location = []
 direction_choice = "LEFT"
 
+def get_new_location(x,y, direction):
+    new_y = new_x = 0
+    if direction == "LEFT":
+        new_y , new_x = y, getX(x-1)
+    if direction == "RIGHT":
+        new_y , new_x = y, getX(x+1)
+    if direction == "UP":
+        new_y , new_x = getY(y - 1), x
+    if direction == "DOWN":
+        new_y , new_x = getY(y + 1), x
+    return new_x, new_y
+    
 def look_ahead(x, y, direction):
     global board
-    if direction == "LEFT":
-        return board[y][getX(x-1)]
-    if direction == "RIGHT":
-        return board[y][getX(x+1)]
-    if direction == "UP":
-        return board[getY(y - 1)][x]
-    if direction == "DOWN":
-        return board[getY(y + 1)][x]
+    new_x, new_y = get_new_location(x, y, direction)
+    return board[new_y][new_x]
+    
         
 # def print_board():
 #     global board
@@ -93,9 +100,9 @@ def seek_at(from_x, from_y, to_x, to_y):
     from_x, from_y = getX(from_x), getY(from_y)
     return board[getY(from_y + to_y)][getX(from_x + to_x)]
     
-def search_structure(possible_directions, size_of_structure = 5):
-    global board, my_location
-    x,y = my_location
+def search_structure(location, possible_directions, size_of_structure = 5):
+    global board
+    x,y = location
     neighbourhood = [[0 for u in range(size_of_structure)] for v in range(size_of_structure)]
     for loop_y in range(size_of_structure):
         for loop_x in range(size_of_structure):
@@ -105,10 +112,14 @@ def search_structure(possible_directions, size_of_structure = 5):
     if size_of_structure == 5:
         structure = np.array([[False,True,True,True,False], [True,True,True,True,True], [False,False,False,False,False], [False,False,False,False,False], [False,False,False,False,False]])
     elif size_of_structure == 7:
-        structure = np.array([[True, True, True, True, True, True, True], [True, True, True, True, True, True, True], [True, True, True, True, True, True, True], [False, False, False, False, False, False, False], [False, False, False, False, False, False, False], [False, False, False, False, False, False, False], [False, False, False, False, False, False, False]])
+        structure = np.array([[True, True, True, True, True, True, True], [True, True, True, True, True, True, True], [False, True, True, True, True, True, False], [False, False, False, False, False, False, False], [False, False, False, False, False, False, False], [False, False, False, False, False, False, False], [False, False, False, False, False, False, False]])
     elif size_of_structure == 9:
-        structure = np.array([[False, True, True, True, True, True, True, True, False], [True, True, True, True, True, True, True, True, True], [True, True, True, True, True, True, True, True, True], [False, True, True, False, True, False, True, True, False], [False, False, False, False, False, False, False, False, False], [False, False, False, False, False, False, False, False, False], [False, False, False, False, False, False, False, False, False], [False, False, False, False, False, False, False, False, False], [False, False, False, False, False, False, False, False, False]])
-        
+        structure = np.array([[True, True, True, True, True, True, True, True, True], [True, True, True, True, True, True, True, True, True], [True, True, True, True, True, True, True, True, True], [False, True, True, True, True, True, True, True, False], [False, False, False, False, False, False, False, False, False], [False, False, False, False, False, False, False, False, False], [False, False, False, False, False, False, False, False, False], [False, False, False, False, False, False, False, False, False], [False, False, False, False, False, False, False, False, False]])
+    elif size_of_structure == 11:
+        structure = np.array([[False, False, True, True, True, True, True, True, True, False, False], [False, False, True, True, True, True, True, True, True, False, False], [False, False, True, True, True, True, True, True, True, False, False], [False, False, True, True, True, True, True, True, True, False, False], [False, False, True, True, True, True, True, True, True, False, False], [False, False, False, False, False, False, False, False, False, False, False], [False, False, False, False, False, False, False, False, False, False, False], [False, False, False, False, False, False, False, False, False, False, False], [False, False, False, False, False, False, False, False, False, False, False], [False, False, False, False, False, False, False, False, False, False, False], [False, False, False, False, False, False, False, False, False, False, False]])
+    elif size_of_structure == 13:
+        # structure = np.array([[False, False, False, True, True, True, True, True, True, True, False, False, False], [False, False, False, True, True, True, True, True, True, True, False, False, False], [False, False, False, True, True, True, True, True, True, True, False, False, False], [False, False, False, True, True, True, True, True, True, True, False, False, False], [False, False, False, True, True, True, True, True, True, True, False, False, False], [False, False, False, True, True, True, True, True, True, True, False, False, False], [False, False, False, False, False, False, False, False, False, False, False, False, False], [False, False, False, False, False, False, False, False, False, False, False, False, False], [False, False, False, False, False, False, False, False, False, False, False, False, False], [False, False, False, False, False, False, False, False, False, False, False, False, False], [False, False, False, False, False, False, False, False, False, False, False, False, False], [False, False, False, False, False, False, False, False, False, False, False, False, False], [False, False, False, False, False, False, False, False, False, False, False, False, False]])
+        structure = np.array([[False, False, False, False, True, True, True, True, True, False, False, False, False], [False, False, False, False, True, True, True, True, True, False, False, False, False], [False, False, False, False, True, True, True, True, True, False, False, False, False], [False, False, False, False, True, True, True, True, True, False, False, False, False], [False, False, False, False, True, True, True, True, True, False, False, False, False], [False, False, False, False, True, True, True, True, True, False, False, False, False], [False, False, False, False, False, False, False, False, False, False, False, False, False], [False, False, False, False, False, False, False, False, False, False, False, False, False], [False, False, False, False, False, False, False, False, False, False, False, False, False], [False, False, False, False, False, False, False, False, False, False, False, False, False], [False, False, False, False, False, False, False, False, False, False, False, False, False], [False, False, False, False, False, False, False, False, False, False, False, False, False], [False, False, False, False, False, False, False, False, False, False, False, False, False]])
     print("neigh", neighbourhood, file=sys.stderr)
     dir_weight = {}
     dir_weight["UP"] = np.sum(neighbourhood * structure == 0) 
@@ -117,17 +128,26 @@ def search_structure(possible_directions, size_of_structure = 5):
     dir_weight["RIGHT"] = np.sum(np.flip(neighbourhood.T, 0) * structure == 0)
     
     print("success %", dir_weight, file=sys.stderr)
-    sorted_keys = sorted(dir_weight, key=lambda k: dir_weight[k])[::-1]
-    result = []
-    for s in sorted_keys:
-        if s in possible_directions:
-            result.append(s)
+    # sorted_keys = sorted(dir_weight, key=lambda k: dir_weight[k])[::-1]
+    
+    result = dir_weight.copy()
+    for s in result:
+        if s not in possible_directions:
+            result[s] = 0
     return result
     
 def deploy_strat():
     print("DEPLOY")
 
 
+def condition_to_fire(direction_costs, sorted_directions):
+    global direction_choice, my_location
+    
+    if sorted_directions[0] == direction_choice and look_ahead(my_location[0], my_location[1], direction_choice) == 1:
+        if direction_costs[sorted_directions[0]] - direction_costs[sorted_directions[1]] > 4 and helper_bots > 0:
+            return True
+    return False
+    
 def move(helper_bots):
     global my_location, direction_choice
     possible_directions = set(direction.values())
@@ -149,9 +169,12 @@ def move(helper_bots):
     #         possible_directions = possible_directions - set(direction_choice)
             
     # structure sum logic
-    better_directions = search_structure(possible_directions, 7)
-    print("bad",better_directions, file=sys.stderr)
-    if better_directions[0] == direction_choice and look_ahead(my_location[0], my_location[1], direction_choice) == 1 and helper_bots >0:
+    direction_costs = search_structure(my_location, possible_directions, 13)
+    sorted_keys = sorted(direction_costs, key=lambda k: direction_costs[k])[::-1]
+    
+    better_directions = list(sorted_keys)
+    print("bad",direction_costs, file=sys.stderr)
+    if condition_to_fire(direction_costs, sorted_keys):
         deploy_strat()
         moved = 2
     else:
